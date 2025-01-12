8d9832f8d9bf807cab9635e428f74972475bff6c
kaavyarao05 <kaavyarao05@gmail.com>
2025-01-12 18:11:37 +0530

    feat: header css in globalcss

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import banner from '@/public/blue-top-notch.png';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <header
+      style={{backgroundImage: `url(${banner.src})`}}>
+      <div className="drop-shadow-md mx-5 py-40 sm:py-52 md:py-72 duration-500">
+        <h1 className="text-5xl sm:text-5xl duration-300 text-center">About</h1>
+      </div>
+     </header>
+     <h2 className='pt-20 pl-10 text-xl font-bold text-cyan font-mono'>
+     The Story Behind Top Notch Cocktails and Mocktails
+     </h2>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <div>
      <header
-      className="text-center text-white bg-center bg-no-repeat bg-cover w-full"
-      style={{backgroundImage: `url(${banner.src})`}}>
+     style={{backgroundImage: `url(${banner.src})`}}>
       <div className="drop-shadow-md mx-5 py-40 sm:py-52 md:py-72 duration-500">
         <h1 className="text-3xl sm:text-5xl duration-300 text-center">Raise Your Glass to<br/>Unforgettable Moments!</h1>
         <h3 className='italic mb-10'> Crafting unique cocktail and mocktail experiences for every celebration.</h3>
