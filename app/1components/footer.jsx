e8783daf8971758d0be5f1d87fc5119b0ab1a3a2
kaavyarao05 <kaavyarao05@gmail.com>
2025-01-16 16:28:43 +0530

    feat: address + working hours responsive

--- app/1components/footer.jsx
+++ app/1components/footer.jsx
@@ -5,22 +5,22 @@ import Logo from '@/public/topNotch.png';
 const footer = () => {
   return (
     <div className='mt-10 pb-5 footer'>
-        <div>
-          <div className='py-7 italic text-center'>
+        <div className='grid sm:grid-cols-2'>
+          <div className='py-7 pl-5 italic text-center row-span-2 mx-auto'>
               <Image src={Logo} width={80} alt='' className='mx-auto m-3'/>
               &quot;Crafting unforgettable experiences,<br/>one drink at a time.&quot;
           </div>
-          <div>
+          <div className='sm:mt-10 sm:pl-0 pl-14'>
             <h3>Address</h3>
             <p>[Your Business Location]</p>
           </div>
-          <div>
+          <div className='my-5 sm:mt-10 sm:pl-0 pl-14'>
             <h3>Working Hours</h3>
             <p>[Your Business Hours]</p>
           </div>
           
         </div>
-        <div className='mt-5 w-full bg-cyan text-center'>
+        <div className='w-full bg-cyan text-center'>
             Connect with us
         </div>
     </div>
