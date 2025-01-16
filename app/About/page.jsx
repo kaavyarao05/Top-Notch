a94304831a614c3461abdcc783e17385768c5f4c
kaavyarao05 <kaavyarao05@gmail.com>
2025-01-16 16:59:48 +0530

    feat: adjusted padding of header and about

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -6,21 +6,21 @@ const About = () => {
     <div>
       <header
       style={{backgroundImage: `url(${banner.src})`}}>
-      <div className="drop-shadow-md mx-5 py-40 sm:py-52 md:py-72 duration-500">
-        <h1 className="text-5xl sm:text-5xl duration-300 text-center">About</h1>
+      <div className="drop-shadow-lg mx-5 py-36 sm:py-40 md:py-56 duration-500">
+      <h1 className="font-extrabold text-4xl md:text-6xl duration-300 text-center">
+        ABOUT
+      </h1>
       </div>
      </header>
-     <h2 className='pt-20 pl-10 text-xl font-bold text-cyan font-mono'>
+     <h2 className='pt-20 pl-[11%] text-xl font-bold text-cyan font-mono'>
      The Story Behind Top Notch Cocktails and Mocktails
      </h2>
-     <div className='pl-14 pt-5 italic space-y-3 text-slate-200'>
+     <div className='pl-[15%] pr-[11%] pt-5 italic space-y-3 text-slate-200'>
      <p>
-     At Top Notch Cocktails and Mocktails, we believe that every celebration deserves the perfect drink to match its spirit.
-     </p>
-     <p>
-      Based in Mangalore, we bring a blend of creativity, passion, and professionalism to elevate your special occasions.
-     </p>
-     <p>
+     At Top Notch Cocktails and Mocktails, we believe that every celebration deserves the perfect drink to match its spirit.&nbsp;
+     <span className='block md:hidden'/>
+     Based in Mangalore, we bring a blend of creativity, passion, and professionalism to elevate your special occasions.
+     <span className='block md:hidden'/>
      From weddings to corporate events, sangeets, and rose ceremonies, our bespoke cocktail and mocktail counters promise to leave your guests delighted.
      </p>
      </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div>
      <header
      style={{backgroundImage: `url(${banner.src})`}}>
-      <div className="drop-shadow-md mx-5 py-40 sm:py-52 md:py-72 duration-500">
-        <h1 className="font-extrabold text-4xl md:text-6xl xl-8xl duration-300 text-center">Raise Your Glass to<br/>Unforgettable Moments!</h1>
+      <div className="drop-shadow-lg mx-5 py-36 sm:py-40 md:py-56 duration-500">
+        <h1 className="font-extrabold text-3xl md:text-5xl xl-xl duration-300 text-center">RAISE YOUR GLASS TO<br/>UNFORGETTABLE MOMENTS!</h1>
         <h3 className='italic mb-10'> Crafting unique cocktail and mocktail experiences for every celebration.</h3>
         <div className='space-y-12 grid w-fit mx-auto'>
             <a 
