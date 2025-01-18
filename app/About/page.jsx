76d453fa80657cd93113fc67e011813118d29fe0
kaavyarao05 <kaavyarao05@gmail.com>
2025-01-18 23:54:08 +0530

    feat: video i nabout

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -4,13 +4,15 @@ import banner from '@/public/blue-top-notch.png';
 const About = () => {
   return (
     <div>
+      <video autoPlay muted playsInline loop>
+          <source src="https://ik.imagekit.io/kaavyarao05/TopNotch/topNotchBlue.mp4?updatedAt=1737224212933"/>
+        </video>
       <header
-      style={{backgroundImage: `url(${banner.src})`}}>
-      <div className="drop-shadow-lg mx-5 py-36 sm:py-40 md:py-56 duration-500">
-      <h1 className="font-extrabold text-4xl md:text-6xl duration-300 text-center">
+      style={{backgroundImage: `url("https://ik.imagekit.io/kaavyarao05/TopNotch/topNotchBlue.mp4")`}}>
+      <h1 className="-mt-[30%] vincendoFont font-extrabold text-4xl md:text-6xl duration-300 text-center">
         ABOUT
       </h1>
-      </div>
+      <div className='mb-[30%]'/>
      </header>
      <h2 className='pt-20 pl-[11%] text-xl font-bold text-cyan font-mono'>
      The Story Behind Top Notch Cocktails and Mocktails
@@ -19,11 +21,23 @@ const About = () => {
      <p>
      At Top Notch Cocktails and Mocktails, we believe that every celebration deserves the perfect drink to match its spirit.&nbsp;
      <span className='block md:hidden'/>
-     Based in Mangalore, we bring a blend of creativity, passion, and professionalism to elevate your special occasions.
+     Based in Mangalore, we bring a blend of creativity, passion, and professionalism to elevate your special occasions.&nbsp;
      <span className='block md:hidden'/>
      From weddings to corporate events, sangeets, and rose ceremonies, our bespoke cocktail and mocktail counters promise to leave your guests delighted.
      </p>
      </div>
+     <h2 className='pt-20 pl-[11%] text-xl font-bold text-cyan font-mono'>
+     Why Choose Us?
+     </h2>
+     <div className='pl-[15%] pr-[11%] pt-5 italic space-y-3 text-slate-200'>
+      <ul className='list-disc'>
+            <li>Expert mixologists with years of experience</li>
+            <li>Customized menus to suit your event theme</li>
+            <li>Premium ingredients and exquisite presentation</li>
+            <li>Exceptional service that ensures a seamless experience</li>
+      </ul>
+     </div>
+     
     </div>
   )
 }
